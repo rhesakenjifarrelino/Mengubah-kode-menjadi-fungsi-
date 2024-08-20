04165f09770410eb0acf3c58cbaca6e29e49d071
rhesakenjifarrelino <kenjiryu123@gmail.com>
2024-08-20 08:46:40 +0700

    Create code1.py

---
+++ code1.py
@@ -0,0 +1,29 @@
+import discord
+from passgen import gen_pass
+
+
+# Variabel intents menyimpan hak istimewa bot
+intents = discord.Intents.default()
+# Mengaktifkan hak istimewa message-reading
+intents.message_content = True
+# Membuat bot di variabel klien dan mentransfernya hak istimewa
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'Kita telah masuk sebagai {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('$halo'):
+        await message.channel.send("Hi!")
+    elif message.content.startswith('$bye'):
+        await message.channel.send("\\U0001f642")
+    elif message.content.startswith('genpass'):
+        await message.channel.send(gen_pass(10))  
+    else:
+        await message.channel.send(message.content)
+
+client.run("Masukan Token Anda")
